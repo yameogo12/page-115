e9f3a7372eb4cceee4e17fc5a7ec5a832282871d
samuel <samuelsyameogo@gmail.com>
2020-02-17 10:22:35 -0800

    boolean
    true or false

---
+++ JS 6/JavaScript-6.js
@@ -0,0 +1,39 @@
+function my_Function(){
+	document.getElementById("Test").innerHTML="onclick";
+}
+
+document.write(2E310);
+document.write(-3E310);
+
+// boolean
+document.write(10> 2);
+document.write(10 < 2);
+console.log(2 +2);
+document.write("10" + 5);
+// double Equal signs
+
+document.write(10==10);
+document.write(3 == 11);
+
+X = 10;
+Y = 10;
+document.write(X===Y);
+
+A = "Magnus";
+B = "Magnus";
+document.write(A === B);
+
+// Logical Operators
+
+document.write(5 > 2 && 10 > 4);
+document.write(5>10 && 10 >4);
+document.write(5 > 10 || 10 > 4);
+document(5 > 10 || 10 > 20);
+
+// Not Operator
+function not_Function(){
+	document.getElementById("Not").innerHTML =!(5>10);
+};
+function not_Function(){
+	document.getElementById("Not").innerHTML = ! (20>10);
+};
